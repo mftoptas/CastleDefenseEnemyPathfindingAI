1af44867e0150fd3cad5518714a1bdfa48dcecac
Muhammet Furkan Toptaş <m.furkantoptas@gmail.com>
2022-08-18 21:16:44 +0300

    Continuing to develop the game
    Continuing to develop the game

--- Assets/Enemy/EnemyMover.cs
+++ Assets/Enemy/EnemyMover.cs
@@ -4,44 +4,50 @@ using UnityEngine;
 
 [RequireComponent(typeof(Enemy))]
 public class EnemyMover : MonoBehaviour
-{
-    [SerializeField] List<Tile> path = new List<Tile>();
+{  
     [SerializeField] [Range(0f, 5f)] float speed = 0.5f; // Enemy speed coefficient.
 
+    List<Node> path = new List<Node>();
+
     Enemy enemy;
+    GridManager gridManager;
+    Pathfinder pathfinder;
 
     void OnEnable()
     {
-        FindPath();
         ReturnStart();
-        StartCoroutine(FollowPath()); // The execution of a coroutine can be paused at any point using the yield statement. When a yield statement is used, the coroutine pauses execution and automatically resumes at the next frame.
+        RecalculatePath(true);
     }
 
-    void Start()
+    void Awake()
     {
         enemy = GetComponent<Enemy>();
+        gridManager = FindObjectOfType<GridManager>();
+        pathfinder = FindObjectOfType<Pathfinder>();
     }
 
-    void FindPath()
+    void RecalculatePath(bool resetPath)
     {
-        path.Clear(); // So when we find a path, we're going to clear the existing one and then add a new one.
+        Vector2Int coordinates = new Vector2Int();
 
-        GameObject parent = GameObject.FindGameObjectWithTag("Path"); // This will then return the path parent object.
-
-        foreach(Transform child in parent.transform) // What this will do now is find that parent object that we've tagged with the path and loop through all of its children in order.
+        if(resetPath)
         {
-            Tile waypoint = child.GetComponent<Tile>();
-            
-            if(waypoint != null)
-            {
-                path.Add(waypoint); // Add waypoint to the path.
-            }
+            coordinates = pathfinder.StartCoordinates;
         }
+        else
+        {
+            coordinates = gridManager.GetCoordinatesFromPosition(transform.position);
+        }
+
+        StopAllCoroutines();
+        path.Clear(); // So when we find a path, we're going to clear the existing one and then add a new one.
+        path = pathfinder.GetNewPath(coordinates);
+        StartCoroutine(FollowPath()); // The execution of a coroutine can be paused at any point using the yield statement. When a yield statement is used, the coroutine pauses execution and automatically resumes at the next frame.
     }
 
     void ReturnStart()
     {
-        transform.position = path[0].transform.position; // It's going to move our enemy into the first waypoint.
+        transform.position = gridManager.GetPositionFromCoordinates(pathfinder.StartCoordinates); // It's going to move our enemy into the first waypoint.
     }
 
     void FinishPath()
@@ -52,10 +58,10 @@ public class EnemyMover : MonoBehaviour
 
     IEnumerator FollowPath()
     {
-        foreach(Tile waypoint in path) // Get started by coroutine.
+        for(int i = 1; i < path.Count; i++) // Get started by coroutine.
         {
             Vector3 startPosition = transform.position;
-            Vector3 endPosition = waypoint.transform.position;
+            Vector3 endPosition = gridManager.GetPositionFromCoordinates(path[i].coordinates);
             float travelPercent = 0f;
 
             transform.LookAt(endPosition); // I am always going to be facing the waypoint that we're heading towards.
--- Assets/Environment/Tile.cs
+++ Assets/Environment/Tile.cs
@@ -5,16 +5,18 @@ using UnityEngine;
 public class Tile : MonoBehaviour
 {
     [SerializeField] Tower towerPrefab;
-
+    
     [SerializeField] bool isPlaceable;
     public bool IsPlaceable { get { return isPlaceable; } } // Now i can call isPlaceable just like any other method from another script.
 
     GridManager gridManager;
+    Pathfinder pathfinder;
     Vector2Int coordinates = new Vector2Int();
 
     void Awake()
     {
         gridManager = FindObjectOfType<GridManager>();
+        pathfinder = FindObjectOfType<Pathfinder>();
     }
 
     void Start()
@@ -29,13 +31,17 @@ public class Tile : MonoBehaviour
             }
         }
     }
-        
+
     void OnMouseDown() // OnMouseDown is called when the user has pressed the mouse button while over the Collider.
     {
-        if(isPlaceable)
+        if(gridManager.GetNode(coordinates).isWalkable && !pathfinder.WillBlockPath(coordinates))
         {
-            bool isPlaced = towerPrefab.CreateTower(towerPrefab, transform.position);
-            isPlaceable = !isPlaced;
+            bool isSuccessful = towerPrefab.CreateTower(towerPrefab, transform.position);
+            if(isSuccessful)
+            {
+                gridManager.BlockNode(coordinates);
+                pathfinder.NotifyReceivers();
+            }
         }
     }
 }
